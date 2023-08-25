cb9d57b359c871538280cb9cfa185b9dec1d48b3
Didula Samaraweera <samaraweeradt.20@uom.lk>
2023-08-25 11:26:19 +0530

    Update create_list.py

--- create_list.py
+++ create_list.py
@@ -156,8 +156,8 @@ def generate_folder(folder_num = 10,whole_file=r"./data_folder/supervised_folder
         file.close()
 
 if __name__ == '__main__':
-    generate_datatxt()
+    #generate_datatxt()
     split_ssl_and_sl()
-    # generate_folder()
+    generate_folder()
     pass
 
